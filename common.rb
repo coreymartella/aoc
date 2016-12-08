dd4ac4714ede36c251723e668045cf222e47962b
Corey Martella <cmartella@miovision.com>
2016-12-08 08:31:35 -0500

    nicer block char for terminal outputs

--- common.rb
+++ common.rb
@@ -1,7 +1,30 @@
 Bundler.require
 require 'pp'
+
+UL = "\x1B[A"
+KL = "\x1B[K"
+def clear_lines(n=1)
+  n.times{print "#{UL}#{KL}";STDOUT.flush}
+end
 class Array
   def histogram
     group_by(&:itself).map { |k, v| [k, v.size] }.to_h
   end
+end
+
+
+def try_p(num,*args)
+  load("common.rb" % num) 
+  load("p%02d.rb" % num) 
+  t = Time.now
+  res = try("p#{num}",*args)
+  IO.popen('pbcopy', 'r+') { |clipboard| clipboard.print res }
+  puts("P#{num}: #{res} in %2.6fs (copied to clipboard)" % (Time.now - t)) 
+  res
+end
+def setup_p(num)
+  File.open("p%02d.rb" % num,'w') do |f|
+    f.puts "load 'common.rb'"
+    f.puts "input = File.read(\"p#{'%02d' % num}.txt\")\n"
+  end unless File.exists?("p%03d.rb" % num)
 end
